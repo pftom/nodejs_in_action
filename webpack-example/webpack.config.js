4ec7891b592d3315c35f760b1faf0f5d595b4f05
黄巍 <huan1043269994@gmail.com>
2017-12-29 17:29:11 +0800

    [ADD] completed webpack.

--- webpack-example/webpack.config.js
+++ webpack-example/webpack.config.js
@@ -2,5 +2,9 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: 
-}
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
